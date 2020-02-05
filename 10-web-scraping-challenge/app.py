5795c2bd8ec6b5ae021a58881ac3dc4f1feb1ec0
jamezxiaozhu <jamezxiaozhu@me.com>
2020-02-05 14:06:27 -0500

    added draft flask app

---
+++ 10-web-scraping-challenge/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import pymongo
+
+app = Flask(__name__)
+
+# setup mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# connect to mongo db and collection
+db = client.mars_info
+collection = db.scrape_info
+
+
+@app.route("/")
+def index():
+    # write a statement that finds all the items in the db and sets it to a variable
+    inventory = list(db.collection.find())
+    print(inventory)
+
+    # render an index.html template and pass it the data you retrieved from the database
+    return render_template("index.html", inventory=inventory)
+
+@app.route("/scrape")
+def scrape():
+    from scrape_mars.py import scape
+    scrape()
+
+if __name__ == "__main__":
+    app.run(debug=True)
