7701af3d1aecde535a3899fdbb58b3cabe821163
jamezxiaozhu <jamezxiaozhu@me.com>
2020-02-18 15:52:52 -0500

    added bonus draft codes

--- 15-plotly-challenge/static/js/app.js
+++ 15-plotly-challenge/static/js/app.js
@@ -17,22 +17,22 @@ function buildMetadata(sample) {
 
 
     // BONUS: Build the Gauge Chart
-    function buildGauge(dataInput){
-      var data = [
-        {
-          domain: { x: [0, 1], y: [0, 1] },
-          value: dataInput,
-          title: { text: "Scrubs per Week" },
-          type: "indicator",
-          mode: "gauge+number"
-        }
-      ];
+    // function buildGauge(dataInput){
+    //   var data = [
+    //     {
+    //       domain: { x: [0, 1], y: [0, 1] },
+    //       value: dataInput,
+    //       title: { text: "Scrubs per Week" },
+    //       type: "indicator",
+    //       mode: "gauge+number"
+    //     }
+    //   ];
       
-      var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-      Plotly.newPlot('gauge', data, layout);
+    //   var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+    //   Plotly.newPlot('gauge', data, layout);
 
-    }
-    buildGauge(sample.WFREQ);
+    // }
+    // buildGauge(sample.WFREQ);
     
     
   }
